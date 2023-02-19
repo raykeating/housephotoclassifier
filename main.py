4b9aba74ec1bf8e5c82b7fe9c2bc65039dd86821
Ray <raykeating13@gmail.com>
2023-02-19 14:26:14 -0500

    allow multiple images to be sent to /predict route

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torchvision
 import torchvision.transforms as transforms
 import flask
 from PIL import Image
+from flask_cors import CORS
 
 # Load the model
 model = torchvision.models.resnet18(pretrained=False)
@@ -33,7 +34,7 @@ def predict(image):
     classes = ['bathroom', 'bedroom', 'dining_room', 'exterior', 'kitchen', 'living_room']
 
     # if the confidence is less than 0.5, return 'unknown'
-    prediction = classes[torch.argmax(probs)] if torch.max(probs) > 0.5 else 'other'
+    prediction = classes[torch.argmax(probs)] if torch.max(probs) > 0.6 else 'other'
 
     probs = probs.detach().cpu().numpy().tolist()[0]
     probs = {classes[i]: probs[i] for i in range(len(classes))}
@@ -42,16 +43,22 @@ def predict(image):
 
 # Create the Flask app
 app = flask.Flask(__name__)
+CORS(app, resources={r"/*": {"origins": ["http://localhost:3000"]}})
 
 @app.route("/predict", methods=["POST"])
 def predict_api():
-    print('hi')
-    # Retrieve the image from the request
-    image = Image.open(flask.request.files["image"])
 
-    # Make the prediction
-    prediction, probs = predict(image)
-    response = {'prediction': prediction, 'probs': probs}
+    predictions = []
+
+    for image in flask.request.files.getlist("image"):
+        # Read the image via file.stream
+        image = Image.open(image.stream)
+
+        # Make the prediction
+        prediction, probs = predict(image)
+        predictions.append({'prediction': prediction, 'probs': probs})
+    
+    response = {'predictions': predictions}
 
     # Convert the prediction to JSON and return it
     return flask.jsonify(response)
